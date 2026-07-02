5aa4be148552f3e06722a13506eeb6b5f571eb1c
Mayur Sahare <mayursahare2024@gmail.com>
2026-07-02 09:32:34 +0530

    tech debt: address issue #1304 (fixes #1304)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,3 +22,5 @@ export default function App() {
         </BrowserRouter>
     );
 }
+
+// TODO: Complete full implementation for #1304 - Enhancement: Migrate LocalStorage to IndexedDB for auditHistory storage
